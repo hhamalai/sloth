9c936f797c5823cbbcb5aa68240000c4bfb8158f
Harri Hämäläinen <hhamalai@iki.fi>
2015-10-21 11:35:04 +0300

    ...

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var temporal = require('temporal')
 
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -12,7 +13,6 @@ app.get('/sleep/:seconds', function (req, res) {
     });
 });
 
-var server = app.listen(80, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
